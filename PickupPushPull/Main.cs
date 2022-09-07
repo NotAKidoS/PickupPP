4d67f19b2029196701e7a3c5d87aa1e0a6293a17
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2022-09-06 20:23:25 -0500

    Added scuffed rotation.
    GitHub desktop kinda ate the entire project so I recovered it from a previous build usin dnSpy. Code looks very shit but it works.

--- PickupPushPull/Main.cs
+++ PickupPushPull/Main.cs
@@ -1,4 +1,5 @@
-﻿using ABI_RC.Core.Player;
+﻿using ABI.CCK.Components;
+using ABI_RC.Core.Player;
 using ABI_RC.Core.Savior;
 using HarmonyLib;
 using MelonLoader;
@@ -6,67 +7,137 @@ using UnityEngine;
 
 namespace PickupPushPull;
 
-public class PickupPushPull : MelonMod
+internal class PickupPushPull : MelonMod
 {
     private static MelonPreferences_Category melonCategoryPickupPushPull;
     private static MelonPreferences_Entry<float> melonEntryPushPullSpeed;
+    private static MelonPreferences_Entry<float> melonEntryRotateSpeed;
+    private static MelonPreferences_Entry<bool> melonEntryEnableRotation;
 
     public override void OnApplicationStart()
     {
-        melonCategoryPickupPushPull = MelonPreferences.CreateCategory(nameof(PickupPushPull));
-        melonEntryPushPullSpeed = melonCategoryPickupPushPull.CreateEntry("PushPullSpeed", 2f, description: "Up/down on VR joystick or left bumper on Gamepad.");
+        melonCategoryPickupPushPull = MelonPreferences.CreateCategory("PickupPushPull");
+        melonEntryPushPullSpeed = melonCategoryPickupPushPull.CreateEntry<float>("PushPullSpeed", 1f, null, "Up/down on VR joystick or left bumper on Gamepad.", false, false, null, null);
+        melonEntryRotateSpeed = melonCategoryPickupPushPull.CreateEntry<float>("RotateSpeed", 1f, null, null, false, false, null, null);
+        melonEntryEnableRotation = melonCategoryPickupPushPull.CreateEntry<bool>("EnableRotation", false, null, "Hold left trigger in VR or right bumper on Gamepad.", false, false, null, null);
 
         melonCategoryPickupPushPull.SaveToFile(false);
         melonEntryPushPullSpeed.OnValueChangedUntyped += UpdateSettings;
-    }
+        melonEntryRotateSpeed.OnValueChangedUntyped += UpdateSettings;
+        melonEntryEnableRotation.OnValueChangedUntyped += UpdateSettings;
 
+        UpdateSettings();
+    }
     private static void UpdateSettings()
     {
         HarmonyPatches.ppSpeed = melonEntryPushPullSpeed.Value;
+        HarmonyPatches.rotSpeed = melonEntryRotateSpeed.Value;
+        HarmonyPatches.enableRot = melonEntryEnableRotation.Value;
     }
 
     [HarmonyPatch]
     private class HarmonyPatches
     {
 
-        public static float ppSpeed = 2f;
+        public static float ppSpeed = melonEntryPushPullSpeed.Value;
+        public static float rotSpeed = melonEntryRotateSpeed.Value;
+        public static bool enableRot = melonEntryEnableRotation.Value;
 
-        //Gamepad Input Patch
+        public static List<CVRPickupObject> grabbedObjects = new List<CVRPickupObject>();
+
+        private static bool lockedVRInput = false;
+        private static bool lockedGamepadInput = false;
         private static CursorLockMode savedCursorLockState;
-        private static bool gamepadButton1 = false;
+
+        //Grab Patches
         [HarmonyPostfix]
-        [HarmonyPatch(typeof(InputModuleGamepad), nameof(InputModuleGamepad.UpdateInput))]
+        [HarmonyPatch(typeof(CVRPickupObject), "Grab")]
+        private static void AfterGrab(ref CVRPickupObject __instance)
+        {
+            if (__instance.IsGrabbedByMe())
+            {
+                grabbedObjects.Add(__instance);
+            }
+        }
+
+        //Gamepad Patches
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(InputModuleGamepad), "UpdateInput")]
         private static void AfterUpdateInput(ref InputModuleGamepad __instance)
         {
-            bool button1 = Input.GetButton("Controller Left Button");
-            if (button1)
+            bool button = Input.GetButton("Controller Left Button");
+            bool button2 = Input.GetButton("Controller Right Button");
+
+            if (button)
             {
-                if (!gamepadButton1)
+                if (!lockedGamepadInput)
                 {
-                    gamepadButton1 = true;
+                    lockedGamepadInput = true;
                     savedCursorLockState = Cursor.lockState;
                     Cursor.lockState = CursorLockMode.None;
                     PlayerSetup.Instance._movementSystem.disableCameraControl = true;
                 }
+                if (button2 && enableRot)
+                {
+                    foreach (CVRPickupObject cvrpickupObject in grabbedObjects)
+                    {
+                        if (!cvrpickupObject.IsGrabbedByMe())
+                        {
+                            grabbedObjects.Remove(cvrpickupObject);
+                            break;
+                        }
+                        cvrpickupObject.initialRotationalOffset *= Quaternion.AngleAxis(rotSpeed * CVRInputManager.Instance.rawLookVector.y, Vector3.right) * Quaternion.AngleAxis(rotSpeed * CVRInputManager.Instance.rawLookVector.x, Vector3.up);
+                    }
+                    return;
+                }
+
                 CVRInputManager.Instance.objectPushPull += CVRInputManager.Instance.rawLookVector.y * ppSpeed;
             }
             else
             {
-                if (gamepadButton1)
+                if (lockedGamepadInput)
                 {
-                    gamepadButton1 = false;
+                    lockedGamepadInput = false;
                     Cursor.lockState = savedCursorLockState;
                     PlayerSetup.Instance._movementSystem.disableCameraControl = false;
                 }
             }
         }
 
-        //VR Input Patch
+        //VR Patches
         [HarmonyPostfix]
-        [HarmonyPatch(typeof(InputModuleSteamVR), nameof(InputModuleSteamVR.UpdateInput))]
+        [HarmonyPatch(typeof(InputModuleSteamVR), "UpdateInput")]
         private static void AfterUpdateInput(ref InputModuleSteamVR __instance)
         {
-            CVRInputManager.Instance.objectPushPull += CVRInputManager.Instance.scrollValue * ppSpeed;
+            bool button = CVRInputManager.Instance.interactLeftValue > 0.9;
+            if (button && enableRot)
+            {
+                if (!lockedVRInput)
+                {
+                    lockedVRInput = true;
+                    PlayerSetup.Instance._movementSystem.canRot = false;
+                    PlayerSetup.Instance._movementSystem.disableCameraControl = true;
+                }
+                foreach (CVRPickupObject cvrpickupObject in grabbedObjects)
+                {
+                    if (!cvrpickupObject.IsGrabbedByMe())
+                    {
+                        grabbedObjects.Remove(cvrpickupObject);
+                        break;
+                    }
+                    cvrpickupObject.initialRotationalOffset *= Quaternion.AngleAxis(rotSpeed * (CVRInputManager.Instance.floatDirection / 2f), Vector3.right) * Quaternion.AngleAxis(rotSpeed * CVRInputManager.Instance.rawLookVector.x, Vector3.up);
+                }
+            }
+            else
+            {
+                if (lockedVRInput)
+                {
+                    lockedVRInput = false;
+                    PlayerSetup.Instance._movementSystem.canRot = true;
+                    PlayerSetup.Instance._movementSystem.disableCameraControl = false;
+                }
+                CVRInputManager.Instance.objectPushPull += CVRInputManager.Instance.scrollValue * ppSpeed;
+            }
         }
     }
 }
--- PickupPushPull/Properties/AssemblyInfo.cs
+++ PickupPushPull/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Reflection;
 namespace PickupPushPull.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const string Author = "NotAKidoS";
 }
