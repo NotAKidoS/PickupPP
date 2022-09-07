2417762ba069dcad3bdc2d882e06c92a123c2b43
NotAKidoS <37721153+NotAKidOnSteam@users.noreply.github.com>
2022-09-07 01:35:34 -0500

    Added scuffed rotation to main.
    Moved scuffed rotation to main and added Desktop rotation support. (if you have mouse4-5)

--- PickupPushPull/Main.cs
+++ PickupPushPull/Main.cs
@@ -1,8 +1,10 @@
-﻿using ABI_RC.Core.Player;
+﻿using ABI.CCK.Components;
+using ABI_RC.Core.Player;
 using ABI_RC.Core.Savior;
 using HarmonyLib;
 using MelonLoader;
 using UnityEngine;
+using Valve.VR;
 
 namespace PickupPushPull;
 
@@ -10,21 +12,28 @@ public class PickupPushPull : MelonMod
 {
     private static MelonPreferences_Category melonCategoryPickupPushPull;
     private static MelonPreferences_Entry<float> melonEntryPushPullSpeed;
+    private static MelonPreferences_Entry<float> melonEntryRotateSpeed;
+    private static MelonPreferences_Entry<bool> melonEntryEnableRotation;
 
     public override void OnApplicationStart()
     {
         melonCategoryPickupPushPull = MelonPreferences.CreateCategory(nameof(PickupPushPull));
         melonEntryPushPullSpeed = melonCategoryPickupPushPull.CreateEntry("PushPullSpeed", 1f, description: "Up/down on right joystick for VR. Left bumper + Up/down on right joystick for Gamepad.");
+        melonEntryRotateSpeed = melonCategoryPickupPushPull.CreateEntry<float>("RotateSpeed", 1f);
+        melonEntryEnableRotation = melonCategoryPickupPushPull.CreateEntry<bool>("EnableRotation", false, description: "Hold left trigger in VR or right bumper on Gamepad.");
 
         melonCategoryPickupPushPull.SaveToFile(false);
         melonEntryPushPullSpeed.OnValueChangedUntyped += UpdateSettings;
+        melonEntryRotateSpeed.OnValueChangedUntyped += UpdateSettings;
+        melonEntryEnableRotation.OnValueChangedUntyped += UpdateSettings;
 
         UpdateSettings();
     }
-
     private static void UpdateSettings()
     {
         HarmonyPatches.ppSpeed = melonEntryPushPullSpeed.Value;
+        HarmonyPatches.rotSpeed = melonEntryRotateSpeed.Value;
+        HarmonyPatches.enableRot = melonEntryEnableRotation.Value;
     }
 
     [HarmonyPatch]
@@ -32,32 +41,66 @@ public class PickupPushPull : MelonMod
     {
 
         public static float ppSpeed = melonEntryPushPullSpeed.Value;
+        public static float rotSpeed = melonEntryRotateSpeed.Value;
+        public static bool enableRot = melonEntryEnableRotation.Value;
 
-        private static bool lockedGamepadInput = false;
+        //we can grab more than one prop in VR
+        public static List<CVRPickupObject> grabbedObjects = new List<CVRPickupObject>();
+
+        private static bool lockedVRInput = false;
+        private static bool lockedFSInput = false;
         private static CursorLockMode savedCursorLockState;
 
-        //Gamepad Input Patch
+        //Grab Patches
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CVRPickupObject), "Grab")]
+        private static void AfterGrab(ref CVRPickupObject __instance)
+        {
+            if (__instance.IsGrabbedByMe())
+            {
+                grabbedObjects.Add(__instance);
+            }
+        }
+
+        //Gamepad & Desktop Input Patch
         [HarmonyPostfix]
         [HarmonyPatch(typeof(InputModuleGamepad), "UpdateInput")]
-        private static void AfterUpdateInput(ref InputModuleGamepad __instance)
+        private static void AfterUpdateInput(ref bool ___enableGamepadInput)
         {
-            bool button1 = Input.GetButton("Controller Left Button");
+
+            bool button1 = Input.GetButton("Controller Left Button") || Input.GetKey(KeyCode.Mouse4) || Input.GetKey(KeyCode.Mouse3);
+            bool button2 = Input.GetButton("Controller Right Button") || Input.GetKey(KeyCode.Mouse3);
+
             if (button1)
             {
-                if (!lockedGamepadInput)
+                if (!lockedFSInput)
                 {
-                    lockedGamepadInput = true;
+                    lockedFSInput = true;
                     savedCursorLockState = Cursor.lockState;
                     Cursor.lockState = CursorLockMode.None;
                     PlayerSetup.Instance._movementSystem.disableCameraControl = true;
                 }
+                if (button2 && enableRot)
+                {
+                    foreach (CVRPickupObject cvrpickupObject in grabbedObjects)
+                    {
+                        if (!cvrpickupObject.IsGrabbedByMe())
+                        {
+                            grabbedObjects.Remove(cvrpickupObject);
+                            break;
+                        }
+                        cvrpickupObject.initialRotationalOffset *= Quaternion.AngleAxis(rotSpeed * CVRInputManager.Instance.rawLookVector.y, Vector3.right) * Quaternion.AngleAxis(rotSpeed * CVRInputManager.Instance.rawLookVector.x, Vector3.up);
+                    }
+                    return;
+                }
+
                 CVRInputManager.Instance.objectPushPull += CVRInputManager.Instance.rawLookVector.y * ppSpeed;
             }
             else
             {
-                if (lockedGamepadInput)
+                if (lockedFSInput)
                 {
-                    lockedGamepadInput = false;
+                    lockedFSInput = false;
                     Cursor.lockState = savedCursorLockState;
                     PlayerSetup.Instance._movementSystem.disableCameraControl = false;
                 }
@@ -67,9 +110,40 @@ public class PickupPushPull : MelonMod
         //VR Input Patch
         [HarmonyPostfix]
         [HarmonyPatch(typeof(InputModuleSteamVR), "UpdateInput")]
-        private static void AfterUpdateInput(ref InputModuleSteamVR __instance)
+        private static void AfterUpdateInput(ref SteamVR_Action_Boolean ___steamVrButtonBTouch)
         {
-            CVRInputManager.Instance.objectPushPull += CVRInputManager.Instance.scrollValue * ppSpeed;
+            if (!MetaPort.Instance.isUsingVr) return;
+
+            bool button = ___steamVrButtonBTouch.GetState(SteamVR_Input_Sources.LeftHand);
+
+            if (button && enableRot)
+            {
+                if (!lockedVRInput)
+                {
+                    lockedVRInput = true;
+                    PlayerSetup.Instance._movementSystem.canRot = false;
+                    PlayerSetup.Instance._movementSystem.disableCameraControl = true;
+                }
+                foreach (CVRPickupObject cvrpickupObject in grabbedObjects)
+                {
+                    if (!cvrpickupObject.IsGrabbedByMe())
+                    {
+                        grabbedObjects.Remove(cvrpickupObject);
+                        break;
+                    }
+                    cvrpickupObject.initialRotationalOffset *= Quaternion.AngleAxis(rotSpeed * (CVRInputManager.Instance.floatDirection / 2f), Vector3.right) * Quaternion.AngleAxis(rotSpeed * CVRInputManager.Instance.rawLookVector.x, Vector3.up);
+                }
+            }
+            else
+            {
+                if (lockedVRInput)
+                {
+                    lockedVRInput = false;
+                    PlayerSetup.Instance._movementSystem.canRot = true;
+                    PlayerSetup.Instance._movementSystem.disableCameraControl = false;
+                }
+                CVRInputManager.Instance.objectPushPull += CVRInputManager.Instance.scrollValue * ppSpeed;
+            }
         }
     }
 }
--- PickupPushPull/Properties/AssemblyInfo.cs
+++ PickupPushPull/Properties/AssemblyInfo.cs
@@ -17,6 +17,7 @@ using System.Reflection;
     AssemblyInfoParams.Author,
     downloadLink: "https://github.com/NotAKidOnSteam/PickupPP"
 )]
+
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
 [assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
@@ -24,6 +25,6 @@ using System.Reflection;
 namespace PickupPushPull.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.1.0";
+    public const string Version = "1.2.0";
     public const string Author = "NotAKidoS";
 }
